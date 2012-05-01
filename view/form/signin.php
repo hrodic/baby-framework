3d402f7067b673336e6c039136b62b5f1b87f243
root <root@lamp.(none)>
2012-05-01 10:39:15 +0000

    cleaning

--- view/form/signin.php
+++
@@ -1,6 +0,0 @@
-<form class="ajax content full" action="/?c=index&m=signin" method="post" id="form-signin">
-    <input pattern="<?php echo \model\form\Signin::getRules('username', 'regexp'); ?>" required="required" name="username" type="username" placeholder="<?= _('Username'); ?>">
-    <input pattern="<?php echo \model\form\Signin::getRules('password', 'regexp'); ?>" required="required" name="password" type="password" placeholder="<?= _('Password'); ?>">
-    <button type="submit"><?= _('Log in'); ?></button>
-    <a href=""><?= _('Recover password'); ?></a>
-</form>
